8c1d415eba0b38ee4fd35a7586d36ce062bbfae3
francocalvo <dev@francocalvo.ar>
2025-08-28 16:59:32 -0300

    chore: remove unused code

--- hosts/kaitain/default.nix
+++ hosts/kaitain/default.nix
@@ -3,54 +3,11 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports = [
-    ./disko.nix
-  ];
+  imports = [ ./disko.nix ];
 
-  # Disable conflicting modules from nixpkgs
-  # disabledModules = [
-  #   "system/boot/loader/raspberrypi"
-  #   "system/boot/loader/raspberrypi/raspberrypi-builder.sh"
-  # ];
-
-  # networking = {
-  #   hostName = "kaitain";
-  #   enableIPv6 = true;
-  #   firewall = {
-  #     enable = true;
-  #     allowedTCPPorts = [ 
-  #       80    # HTTP
-  #       443   # HTTPS
-  #       51820 # WireGuard
-  #       51821 # WireGuard UI
-  #     ];
-  #     allowedUDPPorts = [
-  #       51820 # WireGuard
-  #       51821 # WireGuard UI
-  #     ];
-  #   };
-  # };
-
-  # services = {
-  #   openssh = {
-  #     enable = true;
-  #     settings = {
-  #       PasswordAuthentication = false;
-  #       KbdInteractiveAuthentication = false;
-  #     };
-  #   };
-  #
-  #   hardware.argonone.enable = true;
-  # };
-
-  # modules = {
-  #   editor = {
-  #     neovim.enable = true;
-  #   };
-  #   services = {
-  #     docker.enable = true;
-  #   };
-  # };
+  services = {
+    openssh = { enable = true; };
+  };
 
   # ARM-specific optimizations
   # nix.settings = {
@@ -63,13 +20,13 @@
   #   cpuFreqGovernor = "performance";
   # };
 
-  # Additional packages for VPN/proxy management
-  # environment.systemPackages = with pkgs; [
+  environment.systemPackages = with pkgs; [
+    neovim
     # libraspberrypi
     # wireguard-tools
     # htop
     # iotop
     # tcpdump
     # nmap
-  # ];
+  ];
 }
